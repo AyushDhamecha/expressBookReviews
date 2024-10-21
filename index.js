3193d0d3544cde9f3edfce3119207b7c55aefef2
Ayush Dhamecha <ayuahjiteshdhamecha@gmail.com>
2024-10-21 19:46:39 +0530

    completed

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const session = require('express-session')
+const customer_routes = require('./router/auth_users.js').authenticated;
+const genl_routes = require('./router/general.js').general;
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+
+app.use("/customer/auth/*", function auth(req, res, next) {
+    if (req.session && req.session.accessToken) {
+        jwt.verify(req.session.accessToken, "fingerprint_customer", (err, decoded) => {
+            if (err) {
+                return res.status(401).json({ message: "Unauthorized: Invalid or expired token" });
+            } else {
+                req.user = decoded;
+                next();
+            }
+        });
+    } else {
+        return res.status(401).json({ message: "Unauthorized: No access token found" });
+    }
+});
+
+ 
+const PORT =5000;
+
+app.use("/customer", customer_routes);
+app.use("/", genl_routes);
+
+app.listen(PORT,()=>console.log("Server is running"));
